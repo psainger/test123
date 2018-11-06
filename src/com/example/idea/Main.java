2a2412b9f4ffb96b5447bfb10e6264c6bbc10dac
Pratibha Tomar <ptomar@nextgen.com>
2018-11-06 15:36:22 -0800

    2nd commit

--- src/com/example/idea/Main.java
+++ src/com/example/idea/Main.java
@@ -2,7 +2,8 @@ package com.example.idea;
 
 public class Main {
 
-    //This is testing source tree
+    //This is testing source tree.
+    //Just another line to check commits
 
     public static void main(String[] args) {
 	// write your code here
