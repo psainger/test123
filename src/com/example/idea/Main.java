b95bfe3b8ae247bfd38758dc995f61123e05344b
Pratibha Tomar <ptomar@nextgen.com>
2018-11-06 15:35:43 -0800

    This is 1st commit

--- src/com/example/idea/Main.java
+++ src/com/example/idea/Main.java
@@ -2,6 +2,8 @@ package com.example.idea;
 
 public class Main {
 
+    //This is testing source tree
+
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello from IntelliJ IDEA!");
